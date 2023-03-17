9ffcb644d9f1a66d58733e6fe71396d0f5695cfd
Gurkaran Singh <sgurkaran24@gmail.com>
2023-03-17 18:33:44 -0400

    final

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < namesArray.length; i++) {
+      const message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+
+  function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+  
+  
